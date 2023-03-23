3fe119153aef9d3612eea434b02783b32f5639df
Myrella Alejandra Mas Bula <82183426+myrellalejandra@users.noreply.github.com>
2023-03-23 11:21:41 -0500

    Add files via upload

--- microservice_product/src/main/java/com/example/nttdata/repository/ProductRepository.java
+++ microservice_product/src/main/java/com/example/nttdata/repository/ProductRepository.java
@@ -1,11 +1,24 @@
 package com.example.nttdata.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.nttdata.model.Product;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
 
+    @Query(value="{ 'accountNumber' : ?0 }", fields="{ 'balance' : 1 }")
+    Optional<Product> findBalanceByAccountNumber(Long accountNumber);
+    
+    @Query(value="{ 'accountNumber' : ?0 }", fields="{ 'creditLimit' : 1 }")
+    Optional<Product> findCreditLimit(Long accountNumber);
+    
+    @Query(value="{ 'accountNumber' : ?0 }", fields="{ 'type' : 1 }")
+    Optional<Product> findProductType(Long accountNumber);
+    
 }
+
